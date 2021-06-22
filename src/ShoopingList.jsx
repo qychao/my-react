de2fe11849aa55238689f338e80e77b8f1024ee6
秦永超 <qinyongchao@dfsoft.com.cn>
2021-06-22 10:33:55 +0800

    project init

---
+++ src/ShoopingList.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class ShoopingList extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>shoppingList list for my first demo</h1>
+            </div>
+        );
+    }
+}
